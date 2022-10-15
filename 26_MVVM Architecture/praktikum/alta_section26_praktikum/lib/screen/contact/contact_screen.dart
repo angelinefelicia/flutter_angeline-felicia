385e14befabdbd18f8d8e7218e6cef31604c4fe1
Angeline Felicia <91734471+angelinefelicia@users.noreply.github.com>
2022-10-15 15:57:07 +0700

    Update praktikum section 26

--- 26_MVVM Architecture/praktikum/alta_section26_praktikum/lib/screen/contact/contact_screen.dart
+++ 26_MVVM Architecture/praktikum/alta_section26_praktikum/lib/screen/contact/contact_screen.dart
@@ -39,6 +39,7 @@ class _ContactScreenState extends State<ContactScreen> {
         backgroundColor: darkpink,
         elevation: 0,
       ),
+      drawer: drawerItem(),
       body: ListView.builder(
         itemCount: modalView.contacts.length,
         itemBuilder: (context, index) {
@@ -73,6 +74,75 @@ class _ContactScreenState extends State<ContactScreen> {
           contact.phone,
           style: const TextStyle(fontSize: 14),
         ),
+      ),
+    );
+  }
+
+  Widget drawerItem() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            accountEmail: Text("+62 852 96922134"),
+            accountName: Text("Angeline Felicia"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/backgrounds/bear.jpg'),
+            ),
+            otherAccountsPictures: [
+              Icon(
+                Icons.mode_night_rounded,
+                color: Color(0xFFFFFFFF),
+              ),
+            ],
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 171, 168),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.group),
+            title: const Text('New Group'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_rounded),
+            title: const Text('Contacts'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ContactScreen(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.phone_enabled_rounded),
+            title: const Text('Calls'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings_rounded),
+            title: const Text('Settings'),
+            onTap: () {},
+          ),
+          const Divider(
+            height: 20,
+            thickness: 1,
+            indent: 0,
+            endIndent: 0,
+            color: Color.fromARGB(255, 233, 233, 233),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_add),
+            title: const Text('Invite Friends'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.info_rounded),
+            title: const Text('Telegram Features'),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
